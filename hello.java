cdc6d2555d049e3fc2ac6d14b15cebb0cbc2ff75
shantanu <shantanusharma54321@gmail.com>
2024-04-01 16:18:50 +0530

    addedd the sum function

--- hello.java
+++ hello.java
@@ -1,8 +1,13 @@
 public class hello 
 {
+    public static void sum(int a,int b)
+    {
+        System.out.println(a+b);
+    }
     public static void main(String args[])
     {
         System.out.println("my name is shantanu sharma ");
+        sum(8, 9);
     }
 
 }
